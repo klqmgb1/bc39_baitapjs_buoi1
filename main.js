4586ce4e92c3d3885f4fd7b871fc465ad7df3ccf
klqmgb1 <nguyenhuu07101999@gmail.com>
2022-10-11 21:17:21 +0700

    upload bai tap js buoi 1

---
+++ main.js
@@ -0,0 +1,63 @@
+// Bai tap 1:
+// Đầu vào: 
+var luong1ngay = 100000;
+var songaylam = 30;
+// Xử lí
+var luong = luong1ngay * songaylam;
+// Đầu ra
+var luongthucte = "Lương thực tế: " + luong;
+console.log(luongthucte)
+
+
+// Bai tập 2: 
+// Đầu vào
+var sothuc1 = 2; 
+var sothuc2 = 3;
+var sothuc3 = 4;
+var sothuc4 = 5;
+var sothuc5 = 6;
+// Xử lí
+var tong = sothuc1 + sothuc2 + sothuc3 + sothuc4 + sothuc5;
+var trungbinh =  tong / 5;
+// Đầu ra
+var giatritrungbinh = "Giá trị trung bình của 5 số là: " + trungbinh;
+console.log(giatritrungbinh);
+
+
+// Bài tập 3:
+// Đầu vào:
+var sotienusd = 10;
+var giausd = 23500;
+// xử lí
+var quydoi = sotienusd + giausd;
+// Đầu ra
+var sotienla = "Số tiền sau khu quy đổi: " + quydoi;
+console.log(sotienla);
+
+
+//Bài tập 4
+//Đầu vào:
+var chieudai = 3;
+var chieurong = 4;
+// Xử lí
+var tongdairong = chieudai + chieurong;
+var dientich = chieudai + chieurong;
+var chuvi = tongdairong * 2;
+//Đầu ra:
+var dientichla = "Diện tích hình chữ nhật là: " + dientich;
+var chuvila = "Chu vi hình chữ nhật là: " + chuvi;
+console.log(dientichla);
+console.log(chuvila);
+
+
+//Bài tập 5:
+//Đầu vào
+var kyso = 22;
+// Xử lí
+var kyso1 = kyso % 10;
+var kyso2 = Math.floor(kyso / 10);
+var tongkyso = kyso1 + kyso2;
+// Đầu ra
+var tong2kyso = "Tổng 2 ký số là: " + tongkyso;
+console.log(tong2kyso)
+
